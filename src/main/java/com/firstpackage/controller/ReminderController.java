26cb19a484574f6c88835a620cbe5463c2c9552c
PirogovAlex <pirogovprbb@gmail.com>
2017-05-03 15:10:35 +0300

    Not good yet

--- src/main/java/com/firstpackage/controller/ReminderController.java
+++ src/main/java/com/firstpackage/controller/ReminderController.java
@@ -18,6 +18,6 @@ public class ReminderController {
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
     public String getReminder(ModelMap model) {
-        return "MyReminder first good deployment not good yet";
+        return "MyReminder good deployment 03.04.2017";
     }
 }
